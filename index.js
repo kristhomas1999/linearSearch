d3c74a78f0f6fe8129ca56b0d3cb7c259429f563
Kris Thomas <kresthomas1914@gmail.com>
2020-02-23 23:48:40 +0000

    Added highest and lowest search algorithms

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let numbers = [3, 8, 5, 2];
 
-function searchArray(arr, x){
+function searchHighest(arr, x){
   x = arr[0];
   for(let i=0; i<arr.length; i++){
     if(arr[i] > x){
@@ -13,4 +13,18 @@ function searchArray(arr, x){
   return x;
 }
 
-searchArray(numbers)
+function searchLowest(arr, x){
+  x = arr[0];
+  for(let i=0; i < arr.length; i++){
+    if(arr[i] < x){
+      x = arr[i];
+      return x;
+    } else {
+      x = arr[0];
+    }
+  }
+  return x;
+}
+
+searchHighest(numbers)
+searchLowest(numbers)
